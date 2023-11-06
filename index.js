6859c1476e8da582804c5e7200f739602920526c
Your Name <you@example.com>
2023-11-06 22:45:12 +0600

    added my job collection

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://marketPlace:BFoFbZU0drJXbwtE@cluster0.khqul4z.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://marketPlace:BFoFbZU0drJXbwtE@cluster0.khqul4z.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: { 
+  serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
@@ -24,7 +24,26 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db("marketPlace").collection("jobs");
+    const jobCollection = client.db("marketPlace").collection("myJobs");
 
+    app.get("/myJobs", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await jobCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/myJobs", async (req, res) => {
+      const myJobs = req.body;
+      console.log(myJobs);
+      const result = await jobCollection.insertOne(myJobs);
+      res.send(result);
+    });
+
+    //jobs
     app.get("/jobs", async (req, res) => {
       const cursor = productCollection.find();
       const result = await cursor.toArray();
@@ -44,7 +63,6 @@ async function run() {
       res.send(result);
     });
 
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -58,9 +76,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Crud is running...");
-  });
+  res.send("Crud is running...");
+});
 
-  app.listen(port, () => {
-    console.log(`Simple Crud is Running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Simple Crud is Running on port ${port}`);
+});
